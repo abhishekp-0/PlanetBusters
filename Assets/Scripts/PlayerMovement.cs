5a035ca1e048a7f3384e26cf3d8bb76f1590546a
abhishekp-0 <abhishekpadhi1764@gmail.com>
2024-10-08 16:21:54 +0530

    minor mistake solved

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SpaceshipMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float thrustPower = 10f;  
     public float movePower = 5f;

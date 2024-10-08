abf874212161880dc3bf8847a6526045f134b8fa
AbhinavMilan <abhinavmilan77@gmail.com>
2024-10-08 14:57:58 +0530

    Planet1

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -6,10 +6,34 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     //public GameObject hitEffect;
+    public float timer = 3f;
+    float t;
+
+    private void Start()
+    {
+        t = timer;
+    }
+    public void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Destroy(gameObject);
+            timer += t;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //GameObject effect = Instantiate(hitEffect,transform.position,quaternion.identity);
         //Destroy(effect,5f);
         Destroy(gameObject);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+            Destroy(gameObject);
+
     }
 }

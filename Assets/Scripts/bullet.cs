2a985bf4f7c466fe4199ae16c7047f5151bb7e9d
abhishekp-0 <abhishekpadhi1764@gmail.com>
2024-10-08 06:16:17 +0530

    player movement and shooting done

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    //public GameObject hitEffect;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hitEffect,transform.position,quaternion.identity);
+        //Destroy(effect,5f);
+        Destroy(gameObject);
+    }
+}
